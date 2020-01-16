2d5c8341ae0de5bcbd9ea2172cc5c8baf67e6f96
Xanonymous <trusaidlin@gmail.com>
2020-01-16 10:31:13 +0800

    init c++

---
+++ cmake-build-debug/CMakeFiles/data_structure.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_structure.dir/main.cpp.o"
+  "data_structure"
+  "data_structure.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/data_structure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
