359586b9c230a9ddf4bb43f5402ffb50b715dbe4
Xanonymous <trusaidlin@gmail.com>
2020-01-16 11:43:11 +0800

    chunc files

---
+++ cmake-build-debug/CMakeFiles/adding_array_elements.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/adding_array_elements.dir/adding_array_elements.cpp.o"
+  "adding_array_elements"
+  "adding_array_elements.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/adding_array_elements.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/adding_array_elements.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for adding_array_elements.
+# This may be replaced when dependencies are built.
